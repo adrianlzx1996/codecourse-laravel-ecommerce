251dc28980a45c433d62e74ba04507ded66aa390
Adrian Leong <leongadrian36@gmail.com>
2022-05-24 01:02:01 +0800

    get all categories to tree

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
 	public function __invoke()
 	{
-		$categories = Category::get();
+		$categories = Category::tree()->get()->toTree();
 
 		return view('home', [
 			'categories' => $categories
