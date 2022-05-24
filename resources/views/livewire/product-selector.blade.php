6be46d689b4e9b7fa3e0a5b6d8abb76002230d6a
Adrian Leong <leongadrian36@gmail.com>
2022-05-25 00:10:32 +0800

    show selected variant details

--- resources/views/livewire/product-selector.blade.php
+++ resources/views/livewire/product-selector.blade.php
@@ -1,5 +1,12 @@
-<div>
+<div class="space-y-6">
     @if ($initialVariation)
         <livewire:variation-dropdown :variations="$initialVariation" />
+    @endif
+
+    @if ($skuVariant)
+        <div class="space-y-6">
+            <div class="font-semibold text-lg">{{ $skuVariant->formattedPrice() }}</div>
+            <x-button wire:click.prevent="addToCart">Add to Cart</x-button>
+        </div>
     @endif
 </div>
