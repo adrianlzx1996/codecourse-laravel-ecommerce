3a6fa6670aefcdf06194f761708a5885da6b8835
Adrian Leong <leongadrian36@gmail.com>
2022-05-24 17:00:16 +0800

    style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="grid gap-4 p-6 overflow-hidden md:grid-cols-2 lg:grid-cols-4">
+            <div class="grid gap-4 overflow-hidden md:grid-cols-2 lg:grid-cols-4">
                 @foreach ($categories as $category)
                     <x-category :category="$category" />
                 @endforeach
