e62708ff058e92818b1eb06d103900c30a568986
Adrian Leong <leongadrian36@gmail.com>
2022-05-24 17:00:24 +0800

    style + href for link

--- resources/views/components/category.blade.php
+++ resources/views/components/category.blade.php
@@ -1,5 +1,7 @@
 <div class="ml-4">
-    {{ $category->title }}
+    <a href="/categories/{{ $category->slug }}" class="{{ $category->depth === 0 ? 'font-bold' : '' }}">
+        {{ $category->title }}
+    </a>
 
     @foreach ($category->children as $child)
         <x-category :category="$child" />
