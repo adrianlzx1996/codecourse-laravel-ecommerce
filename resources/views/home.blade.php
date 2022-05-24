38bb37a4d87da91553cb3f94f23dad44782cda34
Adrian Leong <leongadrian36@gmail.com>
2022-05-24 16:56:49 +0800

    clean up + style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,10 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Home') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    @foreach ($categories as $category)
-                        <x-category :category="$category" />
-                    @endforeach
-                </div>
+            <div class="grid gap-4 p-6 overflow-hidden md:grid-cols-2 lg:grid-cols-4">
+                @foreach ($categories as $category)
+                    <x-category :category="$category" />
+                @endforeach
             </div>
         </div>
     </div>
