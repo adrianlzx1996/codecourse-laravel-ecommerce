67071f94c88df11e97aa2ef4d735d02450554229
Adrian Leong <leongadrian36@gmail.com>
2022-05-24 23:51:14 +0800

    livewire: variation dropdown

---
+++ app/Http/Livewire/VariationDropdown.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Variation;
+
+class VariationDropdown extends Component
+{
+	public $variations;
+	public $selectedVariation;
+
+	public function getSelectedVariationModelProperty()
+	{
+		if (!$this->selectedVariation) return;
+
+		return Variation::find($this->selectedVariation);
+	}
+
+	public function updatedSelectedVariation()
+	{
+	}
+
+	public function render()
+	{
+		return view('livewire.variation-dropdown');
+	}
+}
