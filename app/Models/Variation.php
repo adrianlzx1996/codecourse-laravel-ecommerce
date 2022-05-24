1690c88b72cd15bd20835a2bc54b9613018de705
Adrian Leong <leongadrian36@gmail.com>
2022-05-24 23:33:02 +0800

    Variation belongs to Product

--- app/Models/Variation.php
+++ app/Models/Variation.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
 class Variation extends Model
 {
-    use HasFactory;
+	use HasFactory;
+	use HasRecursiveRelationships;
+
+	public function product()
+	{
+		return $this->belongsTo(Product::class);
+	}
 }
