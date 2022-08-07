b146696078736f7a861a79d5b4fec84c6c1380c9
Adrian Leong <leongadrian36@gmail.com>
2022-08-07 22:00:24 +0800

    setup foreign key

--- database/migrations/2022_08_07_133303_create_stocks_table.php
+++ database/migrations/2022_08_07_133303_create_stocks_table.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
+	use App\Models\Variation;
+	use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -15,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
-			$table->foreignId('variation_id')->constrained();
+			$table->foreignId('variation_id')->constrained('variations')->onDelete('cascade');
 			$table->integer('quantity');
             $table->timestamps();
         });
