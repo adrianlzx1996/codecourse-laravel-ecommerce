5efddc7e87fc116df51f9602c4e4952d8ada64a9
Adrian Leong <leongadrian36@gmail.com>
2022-08-08 22:37:29 +0800

    Cart implementation

---
+++ app/Cart/Cart.php
@@ -0,0 +1,46 @@
+<?php
+
+	namespace App\Cart;
+
+	use App\Cart\Contracts\CartInterface;
+	use App\Models\Cart as ModelsCart;
+	use App\Models\User;
+	use Illuminate\Session\SessionManager;
+
+	class Cart implements CartInterface
+	{
+		public function __construct ( protected SessionManager $session ) {}
+
+		public function exists ()
+		{
+			return $this->session->has(config('cart.session.key'));
+		}
+
+		public function create ( ?User $user = null )
+		: void {
+			$instance = ModelsCart::make();
+
+			if ( $user ) {
+				$instance->user()->associate($user);
+			}
+
+			$instance->save();
+
+			$this->session->put(config('cart.session.key'), $instance->uuid);
+		}
+
+		public function contentsCount ()
+		{
+			return $this->contents()->count();
+		}
+
+		public function contents ()
+		{
+			return $this->instance()->variations;
+		}
+
+		protected function instance ()
+		{
+			return ModelsCart::whereUuid($this->session->get(config('cart.session.key')))->first();
+		}
+	}
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -1,11 +1,29 @@
 <?php
 
-namespace App\Models;
+	namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+	use Illuminate\Database\Eloquent\Factories\HasFactory;
+	use Illuminate\Database\Eloquent\Model;
+	use Illuminate\Support\Str;
 
-class Cart extends Model
-{
-    use HasFactory;
-}
+	class Cart extends Model
+	{
+		use HasFactory;
+
+		public static function booted ()
+		{
+			static::creating(function ( $model ) {
+				$model->uuid = (string)Str::uuid();
+			});
+		}
+
+		public function user ()
+		{
+			return $this->belongsTo(User::class);
+		}
+
+		public function variations ()
+		{
+			return $this->belongsToMany(Variation::class);
+		}
+	}
