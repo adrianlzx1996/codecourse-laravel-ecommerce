fcb3ec0f4375c01c48b5839fd6a159a70098ee2e
Adrian Leong <leongadrian36@gmail.com>
2022-08-08 22:53:17 +0800

    add login + register

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -24,6 +24,18 @@
 				   class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
 					Cart ({{ $this->cart->contentsCount() }})
 				</a>
+
+				@guest
+					<a href="/login"
+					   class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+						Login
+					</a>
+					<a href="/register"
+					   class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+						Register
+					</a>
+				@endguest
+
 				@auth
 					<x-dropdown align="right" width="48">
 						<x-slot name="trigger">
