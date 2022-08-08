3f87bbff9d4e8df7f36adda1547d0fc21a85d215
Adrian Leong <leongadrian36@gmail.com>
2022-08-08 22:35:32 +0800

    clean up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace App\Http\Controllers;
+	namespace App\Http\Controllers;
 
-use App\Models\Category;
-use Illuminate\Http\Request;
+	use App\Models\Category;
 
-class HomeController extends Controller
-{
-	public function __invoke()
+	class HomeController extends Controller
 	{
-		$categories = Category::tree()->get()->toTree();
+		public function __invoke ()
+		{
+			$categories = Category::tree()->get()->toTree();
 
-		return view('home', [
-			'categories' => $categories
-		]);
+			return view('home', [
+				'categories' => $categories,
+			]);
+		}
 	}
-}
